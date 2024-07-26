216ddb0500385b9b473b7c77b87a916275c82dff
Daimeng Chen <daimengchen@gmail.com>
2024-07-26 19:05:15 +0800

    pack state a bit more

--- src/encode.js
+++ src/encode.js
@@ -1,10 +1,18 @@
 export function decodePlayers(datastring) {
-    return Uint8Array.from(atob(datastring), v => v.charCodeAt(0))
+    const packed = Uint8Array.from(atob(datastring), v => v.charCodeAt(0))
+    const unpacked = new Uint8Array(packed.length * 4)
+    for (let i = 0; i < packed.length; i++) {
+        unpacked[i * 4 + 3] = (packed[i] & 3)
+        unpacked[i * 4 + 2] = ((packed[i] >> 2) & 3)
+        unpacked[i * 4 + 1] = ((packed[i] >> 4) & 3)
+        unpacked[i * 4 + 0] = ((packed[i] >> 6) & 3)
+    }
+    return unpacked
 }
 
 export function encodePlayers(players) {
     const len = (players.length + 1) / 2
-    const code = new Uint8Array(len)
+    const code = new Array(len).fill(2)
 
     for (let i = 1; i < len; i++) {
         if (players[2 * i - 1] === players[i - 1]) {
@@ -15,7 +23,13 @@ export function encodePlayers(players) {
             code[i - 1] = 2
         }
     }
-
-    return btoa(String.fromCharCode.apply(null, code))
+    const packed = new Uint8Array(len / 4)
+    for (let i = 0; i < len / 4; i++) {
+        packed[i] |= (code[i * 4 + 0] << 6)
+        packed[i] |= (code[i * 4 + 1] << 4)
+        packed[i] |= (code[i * 4 + 2] << 2)
+        packed[i] |= (code[i * 4 + 3])
+    }
+    return btoa(String.fromCharCode.apply(null, packed))
 }
 
